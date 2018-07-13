7876c51619883922d5aa02eb4f80335df5e26c2b
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-13 18:14:51 +0300

    added config

---
+++ Backend/src/database/Database.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static utils.Config.config;
+
+/**
+ * Created by Antonio Zaitoun on 13/07/2018.
+ */
+public class Database implements AutoCloseable {
+
+    final static int USERNAME = config.get("user").getAsJsonObject().get("max_allowed_sessions").getAsInt();
+    final static long PASSWORD = config.get("user").getAsJsonObject().get("session_time_out").getAsInt();
+    final static String DB_LOCATION = config.get("db").getAsJsonObject().get("access_file_location").getAsString();
+
+    private Connection connection;
+
+    @Override
+    public void close() throws Exception {
+        if(!connection.isClosed())
+            connection.close();
+    }
+
+    Database() throws SQLException {
+        try { 
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+                 connection = DriverManager.getConnection(
+                        "jdbc:sqlserver://minitour.mooo.com;databaseName=country_db",
+                    "sa",
+                    "5hy8ejA9MX9jH9");  
+
+            if(connection!=null) 
+            System.out.println("Database Successfully connected"); 
+             } catch (SQLException | IllegalAccessException | InstantiationException | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        }
+    }
+}
