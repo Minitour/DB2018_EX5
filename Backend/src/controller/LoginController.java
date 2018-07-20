347ba9b93a73fa4b4c573f493582ae7ae7822128
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-21 02:42:19 +0300

    Login ready

--- Backend/src/controller/LoginController.java
+++ Backend/src/controller/LoginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import database.data_access.AccountAccess;
 import database.data_access.SessionAccess;
@@ -13,6 +14,7 @@ import utils.RESTRoute;
 import utils.Utils;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Antonio Zaitoun on 13/07/2018.
@@ -20,42 +22,54 @@ import java.util.List;
 public class LoginController implements RESTRoute {
 
 
+    /**
+     *
+     * @param request The request object.
+     * @param response The response object.
+     * @param body the request body.
+     * @param _ignored no session needed here.
+     * @return
+     * @throws Exception
+     */
     @Override
     public Object handle(Request request, Response response, JsonObject body, Session _ignored) throws Exception {
 
-        long startTime = System.currentTimeMillis();
+        JsonElement emailJson = body.get("EMAIL");
+        JsonElement passwordJson = body.get("PASSWORD");
 
-        String email = body.get("EMAIL").getAsString();
-        String password = body.get("PASSWORD").getAsString();
+        //require not null objects
+        require(emailJson,passwordJson);
 
-        require(email,password);
+        String email = emailJson.getAsString();
+        String password = passwordJson.getAsString();
 
 
-        try(AccountAccess account_db = new AccountAccess();
-            SessionAccess session_db = new SessionAccess(account_db)) {
+        //Always use try-with resources to close the connections automatically at the end of the execution.
 
-            long now = System.currentTimeMillis();
-            System.out.println(now - startTime);
-            startTime = now;
+        try(    //create account access with new connection
+                AccountAccess account_db = new AccountAccess();
+
+                //create session access with existing connection
+                SessionAccess session_db = new SessionAccess(account_db)
+        ) {
 
             //get account with email
             List<Account> accounts = account_db.getById(email,null);
 
-            now = System.currentTimeMillis();
-            System.out.println(now - startTime);
-            startTime = now;
-
-            //
+            //make sure there is only one account that matches.
             if (accounts.size() != 1)
                 throw new Exception("Account not found.");
 
+            //get instance
             Account account = accounts.get(0);
 
             //get account id
             Integer accountId = account.getACCOUNT_ID();
 
+            //check password
             boolean doesMatch = BCrypt.checkpw(password,account.getUSER_PASSWORD());
 
+            //password does not match
             if(!doesMatch)
                 throw new Exception("Password does not match!");
 
@@ -64,10 +78,6 @@ public class LoginController implements RESTRoute {
 
             Session session = new Session(accountId,token);
             session_db.upsert(session);
-
-            now = System.currentTimeMillis();
-            System.out.println(now - startTime);
-
             return JSONResponse.SUCCESS().data(session);
         }catch (Exception e){
             return JSONResponse.FAILURE().message(e.getMessage());
