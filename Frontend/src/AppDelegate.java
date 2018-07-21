c844c912ef2b324aaf39d104fcb71e6e9dfc0d4d
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-21 16:20:35 +0300

    Added frontend project

---
+++ Frontend/src/AppDelegate.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Antonio Zaitoun on 21/07/2018.
+ */
+public class AppDelegate extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
