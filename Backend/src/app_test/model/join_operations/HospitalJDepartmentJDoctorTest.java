f8c9a8f2ed083d2c5d4bd9472ea8391dde829b5c
Unknown <goldfedertomer@gmail.com>
2018-07-25 22:26:23 +0300

    Added Tests + Added classes

--- Backend/src/app_test/model/join_operations/HospitalJDepartmentJDoctorTest.java
+++ Backend/src/app_test/model/join_operations/HospitalJDepartmentJDoctorTest.java
@@ -1,10 +1,8 @@
 package model.join_operations;
 
 import database.data_access.QueriesAccess;
-import model.join.HospitalJoinDepartment;
-import model.join.HospitalJoinDepartmentJoinDoctor;
-import model.join.HospitalJoinHospitalized;
-import model.join.HospitalJoinPerson;
+import model.Person;
+import model.join.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,10 +15,28 @@ public class HospitalJDepartmentJDoctorTest {
 
         QueriesAccess queriesAccess = new QueriesAccess();
 
-        List<HospitalJoinHospitalized> hospitalJoinPeople = queriesAccess.query6();
+        List<PersonJoinCheckedBy> personList = queriesAccess.query5("323232323");
 
-        System.out.println(hospitalJoinPeople.get(0).getMonth());
-        System.out.println(hospitalJoinPeople.get(0).getTotalPayment());
+        if (personList.size() > 0) {
+
+            for (int i = 0; i < 2; i++) {
+                System.out.println(personList.get(i).left().getID());
+                System.out.println(personList.get(i).left().getFirstName());
+                System.out.println(personList.get(i).left().getSurName());
+                System.out.println(personList.get(i).right().getCheckTime());
+                System.out.println(personList.get(i).right().getBloodPressure());
+                System.out.println(personList.get(i).right().getBodyTemp());
+
+
+                //System.out.println(hospitalJoinPeople.get(i).getStatus());
+            }
+
+
+
+        }
+        //h.hospitalID, h.name, d.departmentID, d.departmentName,
+        //m.ManagerName, count(doc.doctorID)as'numOfDoctors'
+        //System.out.println(hospitalJoinPeople.get(0).getTotalPayment());
 
         //hospitalJoinPeople.forEach(System.out::println);
 
