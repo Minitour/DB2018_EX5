98ec6a5a6159c347a3e607995f18d104e23f0d78
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-26 17:42:03 +0300

    Fixed Generic Froms + small backend patch

--- Frontend/src/view/generic/GenericTableView.java
+++ Frontend/src/view/generic/GenericTableView.java
@@ -1,20 +1,29 @@
 package view.generic;
 
+import com.google.gson.annotations.Expose;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import ui.UITableView;
+import view.DialogView;
+import view.DynamicDialog;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
  * Created By Tony on 26/07/2018
  */
-public abstract class GenericTableView<T> extends UITableView<T> implements UIFormView.OnFinish<T>{
+public abstract class GenericTableView<T> extends UITableView<T> implements UIFormView.OnFinish<T>,DynamicDialog.DialogDelegate{
 
-    static int arc = 0;
-    final int counter = arc++;
+    private DynamicDialog dialogView;
+
     /**
      * should show the delete column
      */
@@ -44,10 +53,28 @@ public abstract class GenericTableView<T> extends UITableView<T> implements UIFo
         col_view = new TableColumn<>();
 
         col_delete.setCellFactory(param -> new ButtonCell("Delete", this::onDelete));
-        col_view.setCellFactory(param -> new ButtonCell("View", this::onView));
+        col_view.setCellFactory(param -> new ButtonCell("View", index -> {
+            UIFormView view = onView(index);
+            dialogView = new DynamicDialog(view);
+            dialogView.setTitle("Update Entry");
+            dialogView.getPostiveButton().setText("Update");
+            dialogView
+                    .delegate(this)
+                    .prepare()
+                    .show(this);
+        }));
 
         addButton = new Button("Add");
-        addButton.setOnAction(event -> onInsert());
+        addButton.setOnAction(event -> {
+            UIFormView form = onInsert();
+            dialogView = new DynamicDialog(form);
+            dialogView.setTitle("Add Entry");
+            dialogView.getPostiveButton().setText("Add");
+            dialogView
+            .delegate(this)
+            .prepare()
+            .show(this);
+        });
 
 
         if (update) {
@@ -66,26 +93,24 @@ public abstract class GenericTableView<T> extends UITableView<T> implements UIFo
         }
     }
 
-   
+
     /**
      * Method called for deleting an object at certain index.
      *
      * @param index The index to delete.
      */
-    protected void onDelete(int index) {
+    protected abstract void onDelete(int index);
 
-    }
-
-    protected UIFormView<T> onView(int index) {
-        return null;
-    }
+    protected abstract UIFormView<T> onView(int index);
 
     /**
      * Method called when table requests an insert view.
      */
-    protected UIFormView<T> onInsert() {
-        return null;
-    }
+    protected abstract UIFormView<T> onInsert();
+
+    protected abstract Class<T> classType();
+
+    //=========================================================================================================
 
     public static class ButtonCell extends TableCell {
         final Button cellButton;
@@ -115,5 +140,108 @@ public abstract class GenericTableView<T> extends UITableView<T> implements UIFo
             void didSelectAction(int index);
         }
 
+    }
+
+    //=========================================================================================================
+
+    @Override
+    public void callback(T value) {
+        if(dialogView != null)
+            dialogView.close();
+    }
+
+    //=========================================================================================================
+
+    @Override
+    public void onHook(Map<String, Node> views, DynamicDialog dialog) {
+        dialog.getNegativeButton().setText("Cancel");
+        dialog.getNaturalButton().setText("Done");
+        dialog.setMessage("");
+    }
+
+    @Override
+    public boolean onDone(DynamicDialog dialog) {
+        UIFormView<T> v = (UIFormView<T>) dialog.getContentView();
+        T result = v.result();
+        callback(result);
+        return true;
+    }
+
+    @Override
+    public void onCancel(DynamicDialog dialog) {
+
+    }
+
+    //=========================================================================================================
+
+
+    List<Field> inferedFields;
+    @Override
+    public void layoutSubviews(ResourceBundle bundle) {
+        inferedFields = findFieldsAsList(Expose.class,classType());
+        super.layoutSubviews(bundle);
+    }
+
+    @Override
+    public int numberOfColumns() {
+        return inferedFields.size();
+    }
+
+    @Override
+    public String bundleIdForIndex(int index) {
+        return transform(inferedFields.get(index).getName());
+    }
+
+    @Override
+    public TableColumnValue<T> cellValueForColumnAt(int index) {
+        return p -> {
+            try {
+                inferedFields.get(index).setAccessible(true);
+                return inferedFields.get(index).get(p);
+            } catch (IllegalAccessException e) {
+                return null;
+            }
+        };
+    }
+
+    private List<Field> findFieldsAsList(Class<? extends Annotation> ann, Class<?> cls) {
+        List<Field> list = new ArrayList<>();
+
+        while (cls != null) {
+            for (Field field : cls.getDeclaredFields()) {
+                if (field.isAnnotationPresent(ann)) {
+                    list.add(field);
+                }
+            }
+            cls = cls.getSuperclass();
+        }
+        return list;
+    }
+
+    //=========================================================================================================
+
+    private String transform(String str){
+        return captializeAllFirstLetter(convertCamelCaseToUnderscore(str).replaceAll("_"," "));
+    }
+
+    private String convertCamelCaseToUnderscore(String camelCase){
+        String regex = "([a-z])([A-Z]+)";
+        String replacement = "$1_$2";
+        return camelCase
+                .replaceAll(regex, replacement)
+                .toLowerCase();
+    }
+
+    private String captializeAllFirstLetter(String name) {
+        char[] array = name.toCharArray();
+        array[0] = Character.toUpperCase(array[0]);
+
+        for (int i = 1; i < array.length; i++) {
+            if (Character.isWhitespace(array[i - 1])) {
+                array[i] = Character.toUpperCase(array[i]);
+            }
+        }
+
+        return new String(array);
     }
 }
