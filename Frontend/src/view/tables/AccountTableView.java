07263b571aa39623a67284947c63b1729bf7cc4e
Unknown <goldfedertomer@gmail.com>
2018-07-27 20:11:09 +0300

    Update Account Table View

--- Frontend/src/view/tables/AccountTableView.java
+++ Frontend/src/view/tables/AccountTableView.java
@@ -1,36 +1,70 @@
 package view.tables;
 
 import model.Account;
+import network.api.AccountAPI;
+import view.forms.AccountForm;
 import view.generic.GenericTableView;
 import view.generic.UIFormView;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class AccountTableView extends GenericTableView<Account> {
 
+    private AccountAPI api;
+    private List<Account> list = new ArrayList<>();
+
+    public AccountTableView(boolean delete, boolean update, boolean insert) {
+        super(delete, update, insert);
+    }
 
     @Override
     protected void onDelete(int index) {
-
+        api.delete(list.remove(index),System.out::println);
+        reloadData();
     }
 
     @Override
     protected UIFormView<Account> onView(int index) {
-        return null;
+        return new AccountForm(list.get(index),this);
     }
 
     @Override
     protected UIFormView<Account> onInsert() {
-        return null;
+        return new AccountForm(null,this);
     }
 
     @Override
     protected Class<Account> classType() {
-        return null;
+        return Account.class;
     }
 
     @Override
     public Collection<? extends Account> dataSource() {
-        return null;
+        return list;
+    }
+
+    @Override
+    public void layoutSubviews(ResourceBundle bundle) {
+        super.layoutSubviews(bundle);
+        api = new AccountAPI();
+        reloadDataFromServer();
+    }
+
+    private void reloadDataFromServer(){
+        api.readAll((response, items) -> {
+            list.clear();
+            list.addAll(items);
+            reloadData();
+        });
+    }
+
+    @Override
+    public void callback(Account value) {
+        super.callback(value);
+        api.upsert(value,System.out::println);
+        reloadDataFromServer();
     }
 }
