004674ca4de0baf9f31c7bac07ec7e8c21140c34
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-27 19:54:27 +0300

    Patched some issues with backend and frontend

--- Frontend/src/model/Account.java
+++ Frontend/src/model/Account.java
@@ -19,8 +19,31 @@ public class Account{
     @Expose
     private Short ROLE_ID;
 
+    @Expose
+    private Integer hospitalID;
+
 
     public Account(Integer ACCOUNT_ID) { this.ACCOUNT_ID = ACCOUNT_ID; }
     // This class returns ACCOUNT_ID as InsertedId
 
+
+    public Integer getACCOUNT_ID() {
+        return ACCOUNT_ID;
+    }
+
+    public String getUSER_PASSWORD() {
+        return USER_PASSWORD;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public Short getROLE_ID() {
+        return ROLE_ID;
+    }
+
+    public Integer getHospitalID() {
+        return hospitalID;
+    }
 }
