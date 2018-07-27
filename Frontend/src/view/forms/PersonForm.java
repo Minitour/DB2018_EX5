4fd3f55cb594bf725c440593739b6b5560e8fc43
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-27 03:30:42 +0300

    More documentation

--- Frontend/src/view/forms/PersonForm.java
+++ Frontend/src/view/forms/PersonForm.java
@@ -16,8 +16,7 @@ import java.util.ResourceBundle;
  */
 public class PersonForm extends UIFormView<Person> {
 
-
-    private ObservableList<String> careFacilities;
+    
     public PersonForm(Person p,OnFinish<Person> callback) {
         super(Person.class,p,callback);
     }
@@ -27,26 +26,29 @@ public class PersonForm extends UIFormView<Person> {
         return new JFXTextField();
     }
 
-    @Override
-    public boolean isValid() {
-        return false;
-    }
 
-    @Override
-    public void reset() {
-
-    }
-
-    @Override
-    public void setFormMode(FormMode formMode) {
-
-    }
-
+    /**
+     * This method is called once and is used to declare which fields are of type combo box.
+     *
+     * @return an array of field names that you wish to display as combo box.
+     */
     @Override
     protected String[] comboBoxForFields() {
         return new String[]{"careFacility","bloodType","gender"};
     }
 
+    /**
+     * Here we return an observable list.
+     * Note that this observable list is retained within the form itself inside a hashmap.
+     *
+     * If you wish to return a list that is pulled from a server, then create a reference as a field member of this class.
+     * Then make the api call in the layoutSubviews method and in the completion closure add all the items to the observable list.
+     *
+     * Since this is an observable list we don't have to reload it and so this method is only called once.
+     *
+     * @param fieldName The name of the field which is of type combobox and we wish to display as set of items for it.
+     * @return The observable list for the combo box containing the items we want to display.
+     */
     @Override
     protected ObservableList<String> listForField(String fieldName) {
         switch (fieldName){
@@ -60,13 +62,16 @@ public class PersonForm extends UIFormView<Person> {
         return null;
     }
 
+
+    /**
+     * This method is used to lock certain fields when viewing an existing object.
+     * For example when viewing a person we do not wish for the user to edit the ID field.
+     * In this case we return an array that contains "ID".
+     *
+     * @return A list of field names that will not be updated when in editing mode.
+     */
     @Override
     public String[] inupdateableFields() {
         return new String[]{"ID"};
-    }
-
-    @Override
-    public void layoutSubviews(ResourceBundle bundle) {
-        super.layoutSubviews(bundle);
     }
 }
