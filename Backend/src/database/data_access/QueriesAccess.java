e7e869213b281622de9996bcf5adbe1e0346b398
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-28 23:52:15 +0300

    patched dashboard controller

--- Backend/src/database/data_access/QueriesAccess.java
+++ Backend/src/database/data_access/QueriesAccess.java
@@ -20,40 +20,85 @@ public class QueriesAccess extends DataAccess<DBObject> {
         super(existingDatabase);
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinPerson> query2(){
         return get(HospitalJoinPerson.class,"QUERY_2");
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinDepartmentJoinDoctor> query3() {
         return get(HospitalJoinDepartmentJoinDoctor.class,"QUERY_3");
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinPersonJoinDoctor> query4() {
         return get(HospitalJoinPersonJoinDoctor.class,"QUERY_4");
     }
 
+    /**
+     * TODO: Add query description
+     * @param doctorID
+     * @return
+     */
     public List<PersonJoinCheckedBy> query5(String doctorID) {
         return get(PersonJoinCheckedBy.class,"QUERY_5", doctorID);
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinHospitalized> query6(){
         return get(HospitalJoinHospitalized.class,"QUERY_6");
     }
+
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinDepartment> query7A(){
         return get(HospitalJoinDepartment.class,"QUERY_7A");
     }
-
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinDepartmentJoinDoctor> query7B(){
         return get(HospitalJoinDepartmentJoinDoctor.class,"QUERY_7B");
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<Person> query8(){
         return get(Person.class,"QUERY_8");
     }
 
+    /**
+     * TODO: Add query description
+     * @return
+     */
     public List<HospitalJoinDepartment> query9(){
         return get(HospitalJoinDepartment.class,"QUERY_9");
     }
 
+    /**
+     * TODO: Add query description
+     * @param patientId
+     * @return
+     */
+    public List<Person> query11(String patientId){
+        return get(Person.class,"QUERY_11",patientId);
+    }
 
 }
---
+++ Backend/src/model/DashboardModel.java
@@ -0,0 +1,86 @@
+package model;
+
+import com.google.gson.annotations.Expose;
+import model.join.*;
+
+import java.util.List;
+
+/**
+ * Created By Tony on 28/07/2018
+ */
+public class DashboardModel {
+
+    @Expose
+    private List<HospitalJoinPerson> query2_result;
+
+    @Expose
+    private List<HospitalJoinDepartmentJoinDoctor> query3_result;
+
+    @Expose
+    private List<HospitalJoinPersonJoinDoctor> query4_result;
+
+    @Expose
+    private List<PersonJoinCheckedBy> query5_result;
+
+    @Expose
+    private List<HospitalJoinHospitalized> query6_result;
+
+    @Expose
+    private List<HospitalJoinDepartment> query7A_result;
+
+    @Expose
+    private List<HospitalJoinDepartmentJoinDoctor> query7B_result;
+
+    @Expose
+    private List<Person> query8_result;
+
+    @Expose
+    private List<HospitalJoinDepartment> query9_result;
+
+    @Expose
+    private List<Person> query11_result;
+
+
+    public DashboardModel() {
+    }
+
+    public void setQuery2_result(List<HospitalJoinPerson> query2_result) {
+        this.query2_result = query2_result;
+    }
+
+    public void setQuery3_result(List<HospitalJoinDepartmentJoinDoctor> query3_result) {
+        this.query3_result = query3_result;
+    }
+
+    public void setQuery4_result(List<HospitalJoinPersonJoinDoctor> query4_result) {
+        this.query4_result = query4_result;
+    }
+
+    public void setQuery5_result(List<PersonJoinCheckedBy> query5_result) {
+        this.query5_result = query5_result;
+    }
+
+    public void setQuery6_result(List<HospitalJoinHospitalized> query6_result) {
+        this.query6_result = query6_result;
+    }
+
+    public void setQuery7A_result(List<HospitalJoinDepartment> query7A_result) {
+        this.query7A_result = query7A_result;
+    }
+
+    public void setQuery7B_result(List<HospitalJoinDepartmentJoinDoctor> query7B_result) {
+        this.query7B_result = query7B_result;
+    }
+
+    public void setQuery8_result(List<Person> query8_result) {
+        this.query8_result = query8_result;
+    }
+
+    public void setQuery9_result(List<HospitalJoinDepartment> query9_result) {
+        this.query9_result = query9_result;
+    }
+
+    public void setQuery11_result(List<Person> query11_result) {
+        this.query11_result = query11_result;
+    }
+}
