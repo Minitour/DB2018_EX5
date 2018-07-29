4cf2ebf63810997ad81ac205251fa1e8903d9123
Tony Zaitoun <tony.z.1711@gmail.com>
2018-07-29 23:18:07 +0300

    added dashboard

---
+++ Frontend/src/network/api/DashboardAPI.java
@@ -0,0 +1,40 @@
+package network.api;
+
+import com.google.gson.JsonObject;
+import javafx.application.Platform;
+import network.SharedInstance;
+import utils.AutoSignIn;
+import utils.Constants;
+import utils.Response;
+
+/**
+ * Created By Tony on 29/07/2018
+ */
+public class DashboardAPI {
+
+    public void getData(DashboardCallback callback){
+
+        JsonObject data = new JsonObject();
+
+        data.addProperty("ACCOUNT_ID", AutoSignIn.ID);
+        data.addProperty("SESSION_TOKEN",AutoSignIn.SESSION_TOKEN);
+
+
+        SharedInstance.main.makeRequest(Constants.Routes.updatePassword(), null, data, (json, exception) -> {
+            Response r = new Response(json);
+            r.setException(exception);
+
+            Platform.runLater(()-> {
+                if(exception == null && r.isOK()){
+                    callback.make(r,json);
+                }else
+                    callback.make(r,null);
+            });
+
+        });
+    }
+
+    public interface DashboardCallback {
+        void make(Response response,JsonObject data);
+    }
+}
